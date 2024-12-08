70d238f9071056200bcec54fb03a94ac2eadbabe
moicben <benjamain.georges@gmail.com>
2024-12-08 21:26:23 +0700

    0.2

--- components/Header.js
+++ components/Header.js
@@ -1,11 +1,13 @@
+import Link from 'next/link';
+
 export default function Header({ name, logo }) {
   return (
     <header className="pt-20 pb-12">
       <div className="text-2xl text-center dark:text-white">
-        <a href="/">
-          <img src={logo} alt={`${name} logo`} className="logo mx-auto mb-4" />
-          <span>{name}</span>
-        </a>
+        <Link href="/">
+            <img src={logo} alt={`${name} logo`} className="logo mx-auto mb-4" />
+            <span>{name}</span>
+        </Link>
       </div>
     </header>
   );
